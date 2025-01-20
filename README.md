6bedc75a84acd14ea288d0ae0a6c5f0d4291f065
Louis BIZOUERNE <louis.bizouerne@lifen.fr>
2025-01-20 18:31:25 +0100

    added api

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # tweety-api
-API for tweety
+API for tweety https://github.com/mahrtayyab/tweety
+It only support the search of recent tweets based on a query
+
+Don't forget to change the env var `API_KEY`, `TWITTER_USERNAME` and `TWITTER_PASSWORD` when running the container
+
+```
+git clone https://github.com/lo-bi/tweety-api.git
+cd tweety-api
+docker build -t tweety-api:latest .
+docker run -d -p 8000:8000 -e API_KEY=mysecureapikey -e TWITTER_USERNAME=username -e TWITTER_PASSWORD=password tweety-api:latest
+```
+
+Then run the following command to do a query in X recent posts
+```
+curl -X POST "http://127.0.0.1:8000/searchRecentPosts" \
+-H "Content-Type: application/json" \
+-H "x-api-key: APIKEY" \
+-d '{"query": "cats"}'
+```
--- api.py
+++ api.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Depends, HTTPException, Header
+from pydantic import BaseModel
+from tweety import TwitterAsync
+from tweety.filters import SearchFilters
 
 app = FastAPI()
 
+# Function to check the API key
+def verify_api_key(x_api_key: str = Header(...)):
+    expected_api_key = os.getenv("API_KEY")
+    if x_api_key != expected_api_key:
+        raise HTTPException(status_code=403, detail="Invalid API Key")
+
+# Define the request body model
+class SearchQuery(BaseModel):
+    query: str
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(api_key: str = Depends(verify_api_key)):
+    return {"message": "OK"}
+
+@app.post("/searchRecentPosts")
+async def search_recent_posts(search: SearchQuery, api_key: str = Depends(verify_api_key)):
+    username = os.getenv("TWITTER_USERNAME")
+    password = os.getenv("TWITTER_PASSWORD")
+
+    if not username or not password:
+        raise HTTPException(status_code=500, detail="Twitter username or password not set in environment variables")
+
+    # Initialize Twitter client and sign in
+    twitter_client = TwitterAsync("session")
+    await twitter_client.sign_in(username, password)
+
+    # Ensure the user is authenticated
+    user_info = twitter_client.me
+    if not user_info:
+        raise HTTPException(status_code=403, detail="Twitter authentication failed")
+
+    # Search for tweets based on the query
+    tweets = await twitter_client.search(search.query, filter_=SearchFilters.Latest())
+
+    # Format the tweets for the response
+    tweet_data = [{"tweet": tweet} for tweet in tweets]
+
+    return {"tweets": tweet_data}
